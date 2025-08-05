870b71e4e97927c319a2efa53c4e491efab0588b
kshitij jha <connect.kshitijjha@gmail.com>
2025-08-05 11:35:21 +0530

    Add files via upload

---
+++ Assignment- 01.py
@@ -0,0 +1,16 @@
+#Calculating Basic Mathematical Operations:
+
+a = int(input("Enter the First Number: "))
+b = int(input("Enter the Second Number: "))
+
+print("Addition: ", a+b)
+print("Subtraction: ", a-b)
+print("Multiplication: ", a*b)
+print("Division: ", a/b)
+
+#Creating a personalized Greeting:
+
+f_name= input("Enter your First name: ")
+l_name= input("Enter your Last Name: ")
+
+print("Hello, "+f_name.title()+" "+l_name.title()+"! Welcome to the python program")
